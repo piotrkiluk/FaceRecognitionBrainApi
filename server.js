bd0c0326c6a735dd48d30b86cc7eb0062e161da6
PiotrKiluk <piotrkiluk@gmail.com>
2020-12-16 14:30:11 +0100

    added routings: profile image

--- server.js
+++ server.js
@@ -31,14 +31,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-
     if(req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
         res.json('success');
     } else {
         res.status(404).json('error logging in')
     }
-
 })
 
 app.post('/register', (req, res) => {
@@ -54,12 +52,43 @@ app.post('/register', (req, res) => {
     res.json(database.users[database.users.length-1]);
 })
 
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(element => {
+        if(element.id === id) {
+            found = true;
+            return res.json(element);
+        } 
+    })
+    if(!found) { 
+        return res.status(404).json("no such user");
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(element => {
+        if(element.id === id) {
+            found = true;
+            element.entries++;
+            return res.json(element.entries);
+        } 
+    })
+    if(!found) { 
+        return res.status(404).json("not found");
+    } 
+})
+
 app.listen(3000, () => { 
     console.log('app is running on port 3000');
 })
 
 
 /* 
+
+endPoints:
 
 / --> res = this is working
 /signin --> POST = success/fail
