319a961928614e400c871c116a1906919de00f20
PiotrKiluk <piotrkiluk@gmail.com>
2020-12-27 14:13:36 +0100

    endpoints via controllers

---
+++ controllers/image.js
@@ -0,0 +1,12 @@
+import handleRegister from "./register";
+
+const handleImage = (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id )
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => res.json(entries[0]))
+    .catch(err => res.status(400).json('error getting entries data'))
+}
+
+export default handleImage;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-// const express = require('express');
 import express from 'express';
 import bcrypt from 'bcrypt';
 import cors from 'cors';
 import knex from 'knex';
 import handleRegister from './controllers/register.js';
 import handleSignIn from './controllers/signin.js';
+import handleProfileGet from './controllers/profile.js';
+import handleImage from './controllers/image.js';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const db = knex({
     client: 'pg',
@@ -16,55 +21,14 @@ const db = knex({
     }
 });
 
-db.select('*').from('login')
-    .then(data =>{ 
-        console.log(data)
-        db('login').count('* as loginsCount: ')
-            .then(console.log)}
-    )
+app.get('/', (req, res) => { db.select('*').from('users').then(data => {res.send(data)}) });
+app.listen(3000, () => { console.log('app is running on port 3000') });
 
-db.select('*').from('users')
-    .then(data =>{ 
-        console.log(data)
-        db('users').count('* as usersCount: ')
-            .then(console.log)}
-    )    
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.get('/', (req, res) => {
-    db.select('*').from('users').then(data => {res.send(data)})
-});
-
-//poniżej "dependency injection; przesyłamy do handleRegister oprócz req/res również bazę i szyfrowanie"
+//poniżej dependency injection; przesyłamy do handleRegister oprócz req/res również bazę i szyfrowanie"
 app.post('/signin', (req, res) => { handleSignIn(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) });
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({
-        id: id
-    })
-    .then(user => {
-        user.length ? res.json(user[0]) : res.status(400).json('no such user');
-    })
-    .catch(err => response.status(400).json("error getting profile"));
-});
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id )
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => res.json(entries[0]))
-    .catch(err => res.status(400).json('error getting entries data'))
-});
-
-app.listen(3000, () => { 
-    console.log('app is running on port 3000');
-});
+app.get('/profile/:id', (req, res) => { handleProfileGet(req, res) });
+app.put('/image', (req, res) => { handleImage(req, res) });
 
 /* 
 endPoints:
@@ -75,32 +39,49 @@ endPoints:
 /profile/:userID --> GET = user
 /image --> PUT --> user
 
+dbCkecks:
+
+db.select('*').from('login')
+    .then(data =>{ 
+        console.log(data)
+        db('login').count('* as loginsCount: ')
+            .then(console.log)}
+    );
+
+db.select('*').from('users')
+    .then(data =>{ 
+        console.log(data)
+        db('users').count('* as usersCount: ')
+            .then(console.log)}
+    ); 
+
+old db mockup!
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            password: 'cookies',
+            email: 'john@gmail.com',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            password: 'bananas',
+            email: 'sally@gmail.com',
+            entries: 0,
+            joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
+        }
+    ]
+}
+
 */
-// old db mockup!
-// const database = {
-//     users: [
-//         {
-//             id: '123',
-//             name: 'John',
-//             password: 'cookies',
-//             email: 'john@gmail.com',
-//             entries: 0,
-//             joined: new Date()
-//         },
-//         {
-//             id: '124',
-//             name: 'Sally',
-//             password: 'bananas',
-//             email: 'sally@gmail.com',
-//             entries: 0,
-//             joined: new Date()
-//         }
-//     ],
-//     login: [
-//         {
-//             id: '987',
-//             hash: '',
-//             email: 'john@gmail.com'
-//         }
-//     ]
-// }
